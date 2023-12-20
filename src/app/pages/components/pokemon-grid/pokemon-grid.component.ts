58ca2b0d728c10830c47d54866c85fffebe281d8
Xavier <xavier.ortigosacortes@emeal.nttdata.com>
2023-12-20 19:40:37 +0100

    cards appears when the end is reached

--- src/app/pages/components/pokemon-grid/pokemon-grid.component.ts
+++ src/app/pages/components/pokemon-grid/pokemon-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-grid',
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class PokemonGridComponent {
   pokemonNumbers: number[];
+  currentPage: number = 1;
 
   @Input() maxPokemon: number = 12;
 
@@ -15,5 +16,33 @@ export class PokemonGridComponent {
       { length: this.maxPokemon },
       (_, i) => i + 1
     );
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    if (this.isScrollingNearBottom()) {
+      console.log('END');
+      this.loadMorePokemon();
+    }
+  }
+
+  isScrollingNearBottom(): boolean {
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    const scrollTop =
+      window.scrollY ||
+      document.body.scrollTop + (document.documentElement.scrollTop || 0);
+
+    return scrollTop + windowHeight >= documentHeight - 50;
+  }
+
+  loadMorePokemon(): void {
+    const newPokemonNumbers = Array.from(
+      { length: this.maxPokemon },
+      (_, index) => this.currentPage * this.maxPokemon + index
+    );
+    console.log('new: ', newPokemonNumbers);
+    this.pokemonNumbers = [...this.pokemonNumbers, ...newPokemonNumbers];
+    this.currentPage++;
   }
 }
